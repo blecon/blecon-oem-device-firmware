ede9a8936a12ed51a23771359e04b89ea168d1e8
Nicholas Chen <nick.chen@blecon.net>
2025-08-13 16:14:18 +0100

    Update function docs in header file

--- lib/power_switch/include/power_switch/power_switch.h
+++ lib/power_switch/include/power_switch/power_switch.h
@@ -6,14 +6,15 @@
 #include <zephyr/kernel.h>
 
 /**
- * Call on system power-up. If it is a power-on reset,
- * immediately put the system into off mode. Otherwise,
- * continue normal startup.
+ * Call on system power-up. If it is a power-on or pin-triggered reset,
+ * immediately put the system into off mode. Otherwise, continue normal
+ * startup.
  */
 void power_sys_start();
 
 /**
- * Put the system into off mode and use wake_gpio
- * to wake the system
+ * Put the system into off mode and use the GPIO set in the
+ * blecon,sw-power property in the Devicetree's /chosen node
+ * as the wake-up source.
  */
 void power_off();
